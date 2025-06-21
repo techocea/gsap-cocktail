0d37fb0221f202a3d6e9ef21d160183b4c7c69d0
techocea <dunsfordbright@gmail.com>
2025-06-21 06:55:39 +0530

    implemented the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import gsap from 'gsap';
 import {ScrollTrigger, SplitText} from "gsap/all";
+import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <main className="flex-center h-[100vh]">
-                <h1 className="text-3xl uppercase">HEllo GSAP</h1>
-        </main>
+      <main>
+          <Navbar/>
+      </main>
     )
 }
 export default App;
